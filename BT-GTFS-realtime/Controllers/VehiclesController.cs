f28881e1c6963a21dc5ee70777db1a907a4df1c5
Michael Kropat <michael.kropat@rackspace.com>
2015-06-06 13:02:01 -0400

    Returning VehicleDescriptor

--- BT-GTFS-realtime/Controllers/VehiclesController.cs
+++ BT-GTFS-realtime/Controllers/VehiclesController.cs
@@ -1,26 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
-using BT_GTFS_realtime.Models;
+using transit_realtime;
 
 namespace BT_GTFS_realtime.Controllers
 {
     public class VehiclesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<Item> Get()
+        public IEnumerable<VehicleDescriptor> Get()
         {
-            return new Item[] {
-                new Item {
-                    Id = 123,
-                    Name = "The Yellow Submarine",
-                    Value = 654321,
+            return new VehicleDescriptor[] {
+                new VehicleDescriptor {
+                    id = "yellow",
+                    label = "The Yellow Submarine",
                 }
             };
-            //return new string[] { "value1", "value2" };
         }
     }
 }
