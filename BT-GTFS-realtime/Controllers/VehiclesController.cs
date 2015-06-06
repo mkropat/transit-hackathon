368a3294d6312ba0dc240beca35fe7f8dae6e83b
Michael Kropat <michael.kropat@rackspace.com>
2015-06-06 15:52:33 -0400

    Populate lat/long from web service

--- BT-GTFS-realtime/Controllers/VehiclesController.cs
+++ BT-GTFS-realtime/Controllers/VehiclesController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
+using System.Xml.Linq;
 using transit_realtime;
 
 namespace BT_GTFS_realtime.Controllers
@@ -10,26 +11,33 @@ namespace BT_GTFS_realtime.Controllers
         {
             var client = new BT4U.BT4U_WebServiceSoapClient("BT4U_WebServiceSoap12");
             var response = client.GetCurrentBusInfo();
-
+            var doc = XElement.Parse(response.OuterXml);
 
             var message = new FeedMessage();
             message.header = new FeedHeader();
             message.header.gtfs_realtime_version = "1.0";
             message.header.incrementality = FeedHeader.Incrementality.FULL_DATASET;
 
-            var update = new FeedEntity();
-            message.entity.Add(update);
+            foreach (var vehicleNode in doc.Descendants("LatestInfoTable")) {
 
-            update.id = "1922";
+                if (vehicleNode.Element("Latitude").Value == "0")
+                    continue;
 
-            var vehicle = update.vehicle = new VehiclePosition();
+                var update = new FeedEntity();
 
-            var descriptor = vehicle.vehicle = new VehicleDescriptor();
-            descriptor.label = response.InnerXml;
+                message.entity.Add(update);
 
-            var position = vehicle.position = new Position();
-            position.latitude = 37.24212F;
-            position.longitude = -80.43342F;
+                update.id = vehicleNode.Element("AgencyVehicleName").Value;
+
+                var vehicle = update.vehicle = new VehiclePosition();
+
+                var descriptor = vehicle.vehicle = new VehicleDescriptor();
+
+                var position = vehicle.position = new Position();
+
+                position.latitude = float.Parse(vehicleNode.Element("Latitude").Value);
+                position.longitude = float.Parse(vehicleNode.Element("Longitude").Value);
+            }
 
             return message;
         }
